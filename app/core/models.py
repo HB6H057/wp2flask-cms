14bc022d11129eaae975941d5cea057d98893fc7
hb6h057 <me@hb6.org>
2016-01-06 03:07:21 +0800

    finish models

--- app/core/models.py
+++ app/core/models.py
@@ -6,12 +6,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
+post_tag_table = db.Table(
+    'post_tag',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
     nickname = db.Column(db.String(64))
     password_hash = db.Column(db.String(128))
+
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
 
     @property
     def password(self):
@@ -33,7 +41,7 @@ class Category(db.Model):
     slug = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(128))
 
-    posts = db.Column(db.String(64))
+    posts = db.relationship('Post', backref='category', lazy='dynamic')
 
     def __repr__(self):
         return '<Category %s>' % self.name
@@ -44,9 +52,12 @@ class Post(db.Model):
     description = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
-    categorys = db.Column(db.String(64))
-    user_id = db.Column(db.String(64))
-    tags = db.Column(db.String(64))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    categorys = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    tags = db.relationship('Tag', secondary=post_tag_table,
+                            backref=db.backref('posts', lazy='dynamic')
+                          )
 
     def __repr__(self):
         return '<Post %s>' % self.title
@@ -56,7 +67,7 @@ class Tag(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     slug = db.Column(db.String(64), index=True, unique=True)
 
-    posts = db.Column(db.String(64))
+    # posts m2m
 
     def __repr__(self):
         return '<Tag %s>' % self.name
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app
 
 app = create_app()
 
-app.run(debug=True)
+app.run()
