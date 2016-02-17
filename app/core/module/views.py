4a6c1246221ae3b811a073da9f57ff479fdb5724
hb6h057 <me@hb6.org>
2016-02-17 13:16:26 +0800

    implement module static file

--- app/core/module/views.py
+++ app/core/module/views.py
@@ -1,24 +1,16 @@
 # encoding: utf-8
 from app.core.base import *
+from flask import send_from_directory, request
 
 from . import module
 
-@module.route('/robots.txt')
-def robots_txt():
-    return 'Confidence is a preference for the habitual voyeur of what is known as (parklife)'
-
 @module.route('/sitemap.xml')
-def sitemap_xml():
-    return 'no alarms and no surprises'
+@module.route('/robots.txt')
+@module.route('/sitemap_baidu.xml')
+@module.route('/sitemap.html')
+def robots_txt():
+    return send_from_directory(module.static_folder, request.path[1:])
 
 @module.route('/sitemap.xml.gz')
 def sitemap_xml_gz():
     return 'All goes round again'
-
-@module.route('/sitemap_baidu.xml')
-def sitemap_baidu_xml():
-    return 'A heart thats full up like a landfill, a job that slowly kills you, bruises that wont heal'
-
-@module.route('/sitemap.html')
-def sitemap_html():
-    return 'Open your mouth wide, A universal sigh.'
