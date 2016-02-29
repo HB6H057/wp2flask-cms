8b24df5b0f95e2d84488eea194449b014e663bb2
hb6h057 <me@hb6.org>
2016-02-29 09:09:35 +0800

    uodate BaseSerive

--- app/core/base.py
+++ app/core/base.py
@@ -5,10 +5,53 @@ from app.core.models import User, Post, Category, Tag
 
 class BaseSerive(object):
     @staticmethod
+    def get_newest_posts(slug, model=Post, count=3):
+        """
+        get newest posts
+        """
+        cate = model.query.filter_by(slug=slug).one()
+        posts = cate.posts[:count]
+
+        cate_posts_dict = dict(
+            id=cate.id,
+            slug=cate.slug,
+            name=cate.name,
+            plist=[
+                dict(
+                    id=p.id,
+                    title=p.title,
+                    slug=p.slug,
+                )
+                for p in posts
+            ],
+        )
+
+        return cate_posts_dict
+
+    @staticmethod
+    def get_tag_data(self):
+        """
+        get tag data
+        """
+        tags = Tag.query.all()
+
+        tag_dict = [
+            dict(
+                id=t.id,
+                name=t.name,
+                slug=t.slug,
+                count=t.posts.count()
+            )
+            for t in tags
+        ]
+
+        return tag_dict
+
+    @staticmethod
     def get_cate_data(self):
         """
         get category data
-        TODO: sorted function
+        TODO: finish sorted function
         """
         cates = Category.query.all()
 
@@ -23,6 +66,7 @@ class BaseSerive(object):
         ]
 
         return nav_dict
+
     @staticmethod
     def random_get_data(self, model=Post, count=3):
         """
