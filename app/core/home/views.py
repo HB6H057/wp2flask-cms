f312fd1005a89e4cb878b60689a153e47e3bd7c9
hb6h057 <me@hb6.org>
2017-03-06 17:19:40 +0800

    Delete old code

--- app/core/home/views.py
+++ app/core/home/views.py
@@ -1,11 +1,6 @@
 # encoding: utf-8
-import json
 
-from sqlalchemy import func
-from flask import request
-from flask.views import MethodView
-
-from app.core.views import TemplateView, DetailView, ListView, ModelFieldListView
+from app.core.views import TemplateView, DetailView, ModelFieldListView
 from app.core.models import Category
 from app.core.base import *
 from . import home
@@ -72,48 +67,6 @@ post_view_func = PostView.as_view('post')
 home.add_url_rule('/<string:cslug>/<string:slug>.html', view_func=post_view_func )
 
 
-# @home.route('/')
-# def index():
-#     """
-#     Index page
-#     """
-#     ser = HomeService()
-#
-#     context = dict(
-#         nav=ser.cates,
-#         cps=ser.get_cate_posts(),
-#         hs=ser.get_random_posts(10),
-#         briefs=ser.get_briefs(),
-#         sidebar=dict(
-#             new=ser.get_newest_posts(),
-#             tags=ser.get_tags(),
-#         )
-#     )
-#
-#     # return json.dumps(context)
-#     return render_template(
-#         'index.jinja2',
-#         ct=context
-#     )
-
-
-# @home.route('/category/<string:cslug>')
-# @home.route('/category/<string:cslug>/page/<int:page>')
-# def category(cslug, page=1):
-#     ca = CatePageService(cslug, page)
-#     catepage_data = ca.get_post_list()
-#
-#     context = dict(
-#         nav=ca.cates,
-#         cd=catepage_data,
-#     )
-#
-#     return render_template(
-#         'category.jinja2',
-#         ct=context,
-#     )
-
-
 @home.route('/<int:year>/<string:month>', methods=['GET', 'POST'])
 @home.route('/<int:year>/<string:month>/page/<int:page_num>', methods=['GET', 'POST'])
 def archive(year, month, page_num=1):
@@ -121,47 +74,3 @@ def archive(year, month, page_num=1):
     Archive default page
     """
     return 'High & Dry'
-
-
-# @home.route('/<string:cslug>/<string:pslug>.html', methods=['GET', 'POST'])
-# def post(cslug, pslug):
-#     """
-#     post page
-#     """
-#     pp = PostPageService(cslug, pslug)
-#     post_dict = pp.get_post()
-#     comment_dict_list = pp.get_comments_data()
-#     related_posts = pp.get_related_posts()
-#
-#     context = dict(
-#         nav=pp.cates,
-#         pd=post_dict,
-#         cmds=comment_dict_list,
-#         rel=related_posts,
-#         random=pp.get_random_posts(),
-#     )
-#     # return json.dumps(related_posts)
-#     return render_template(
-#         'post.jinja2',
-#         ct=context
-#     )
-
-
-# @home.route('/tag/<string:tslug>', methods=['GET', 'POST'])
-# @home.route('/tag/<string:tslug>/page/<int:page>', methods=['GET', 'POST'])
-# def tag(tslug, page=1):
-#     """
-#     Tag default page
-#     """
-#     tps = TagPageService(tslug, page)
-#     tagpage_data = tps.get_post_list()
-#
-#     context = dict(
-#         nav=tps.cates,
-#         td=tagpage_data,
-#     )
-#
-#     return render_template(
-#         'tag.jinja2',
-#         ct=context,
-#     )
